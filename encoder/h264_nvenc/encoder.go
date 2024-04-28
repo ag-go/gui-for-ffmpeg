20176176145c472f6dba52e0acbd77ccd4ae5522
Leonid Nikitin <i@kor-elf.net>
2024-04-28 14:43:18 +0500

    Added preset option for libx265. Added preset option for h264_nvenc.

--- encoder/h264_nvenc/encoder.go
+++ encoder/h264_nvenc/encoder.go
@@ -1,13 +1,37 @@
 package h264_nvenc
 
 import (
+	"errors"
 	encoder2 "git.kor-elf.net/kor-elf/gui-for-ffmpeg/encoder"
 )
 
+var Presets = []string{
+	"default",
+	"slow",
+	"medium",
+	"fast",
+	"hp",
+	"hq",
+	"bd",
+	"ll",
+	"llhq",
+	"llhp",
+	"lossless",
+	"losslesshp",
+}
+
 func NewEncoder() encoder2.EncoderContract {
-	parameters := map[string]encoder2.ParameterContract{}
+	parameters := map[string]encoder2.ParameterContract{
+		"preset": newParameterPreset(),
+	}
 	getParams := func(parameters map[string]encoder2.ParameterContract) []string {
-		return []string{"-c:v", "h264_nvenc"}
+		params := []string{"-c:v", "h264_nvenc"}
+
+		if parameters["preset"] != nil && parameters["preset"].IsEnabled() {
+			params = append(params, "-preset", parameters["preset"].Get())
+		}
+
+		return params
 	}
 
 	return encoder2.NewEncoder("h264_nvenc", parameters, getParams)
@@ -18,4 +42,17 @@ func NewData() encoder2.EncoderDataContract {
 	formats := []string{"mp4"}
 	fileType := encoder2.FileType(encoder2.Video)
 	return encoder2.NewData(title, formats, fileType, NewEncoder)
+}
+
+func newParameterPreset() encoder2.ParameterContract {
+	setParameter := func(s string) (string, error) {
+		for _, value := range Presets {
+			if value == s {
+				return value, nil
+			}
+		}
+
+		return "", errors.New("preset not found")
+	}
+	return encoder2.NewParameter("preset", false, "default", setParameter)
 }
--- encoder/libx265/encoder.go
+++ encoder/libx265/encoder.go
@@ -1,11 +1,35 @@
 package libx265
 
-import encoder2 "git.kor-elf.net/kor-elf/gui-for-ffmpeg/encoder"
+import (
+	"errors"
+	encoder2 "git.kor-elf.net/kor-elf/gui-for-ffmpeg/encoder"
+)
+
+var Presets = []string{
+	"ultrafast",
+	"superfast",
+	"veryfast",
+	"faster",
+	"fast",
+	"medium",
+	"slow",
+	"slower",
+	"veryslow",
+	"placebo",
+}
 
 func NewEncoder() encoder2.EncoderContract {
-	parameters := map[string]encoder2.ParameterContract{}
+	parameters := map[string]encoder2.ParameterContract{
+		"preset": newParameterPreset(),
+	}
 	getParams := func(parameters map[string]encoder2.ParameterContract) []string {
-		return []string{"-c:v", "libx265"}
+		params := []string{"-c:v", "libx265"}
+
+		if parameters["preset"] != nil && parameters["preset"].IsEnabled() {
+			params = append(params, "-preset", parameters["preset"].Get())
+		}
+
+		return params
 	}
 
 	return encoder2.NewEncoder("libx265", parameters, getParams)
@@ -16,4 +40,17 @@ func NewData() encoder2.EncoderDataContract {
 	formats := []string{"mp4"}
 	fileType := encoder2.FileType(encoder2.Video)
 	return encoder2.NewData(title, formats, fileType, NewEncoder)
+}
+
+func newParameterPreset() encoder2.ParameterContract {
+	setParameter := func(s string) (string, error) {
+		for _, value := range Presets {
+			if value == s {
+				return value, nil
+			}
+		}
+
+		return "", errors.New("preset not found")
+	}
+	return encoder2.NewParameter("preset", false, "medium", setParameter)
 }
